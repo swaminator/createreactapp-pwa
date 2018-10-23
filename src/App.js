168b79f255299b7a33bc4a73a8b2c532c8ca2cfb
Nikhil Swaminathan <nsswamin@amazon.com>
2018-10-23 10:28:12 -0700

    Changes for demo with Gregory

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload PWA!
+            Edit <code>src/App.js</code> and save to reload PWA 2!
           </p>
           <a
             className="App-link"
